27011f6f11371b909906922729034f9eaf255b9b
mharv <mitch.k.harvey@gmail.com>
2023-12-09 19:49:29 +0000

    experimenting with logger and Component types

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(bevy::log::LogPlugin {
+            // Uncomment this to override the default log settings:
+            // level: bevy::log::Level::TRACE,
+            // filter: "wgpu=warn,bevy_ecs=info".to_string(),
+            ..default()
+        }))
         .add_plugins(HelloPlugin)
         .run();
-}
-
-fn hello_world() {
-    println!("hello world!");
 }
 
 #[derive(Component)]
@@ -17,15 +18,24 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Resource, Debug)]
+struct GreetTimer(Timer, &'static str);
+
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    info!("{}", timer.1);
+    info!("{:?}", timer.0);
+
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("ola {}!", name.0);
+            info!("trace");
+        }
     }
 }
 
@@ -33,8 +43,11 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
-            .add_systems(Update, greet_people)
-            .add_systems(Update, hello_world);
+        app.insert_resource(GreetTimer(
+            Timer::from_seconds(3., TimerMode::Repeating),
+            "greet_timer",
+        ))
+        .add_systems(Startup, add_people)
+        .add_systems(Update, greet_people);
     }
 }
